18232d3107d869fc8fee7c6794f582d729af6c08
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-07-08 15:10:26 -0400

    inicialización de facebook, flowmanager y firebase

--- app/src/main/java/edu/galileo/android/tipcalc/TipCalcApp.java
+++ app/src/main/java/edu/galileo/android/tipcalc/TipCalcApp.java
@@ -2,13 +2,48 @@ package edu.galileo.android.tipcalc;
 
 import android.app.Application;
 
+import com.facebook.FacebookSdk;
+import com.firebase.client.Firebase;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
 /**
  * Created by carlos.gomez on 30/05/2016.
  */
 public class TipCalcApp extends Application {
     private final static String ABOUT_URL = "http://www.facebook.com/ancho85";
 
-    public String getAboutUrl(){
+    public String getAboutUrl() {
         return ABOUT_URL;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initFacebook();
+        initDB();
+        initFirebase();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        destroyDB();
+    }
+
+    private void initFacebook() {
+        FacebookSdk.sdkInitialize(this);
+    }
+
+    private void initDB() {
+        FlowManager.init(this);
+    }
+
+    private void destroyDB() {
+        FlowManager.destroy();
+    }
+
+    private void initFirebase() {
+        Firebase.setAndroidContext(this);
+        Firebase.getDefaultConfig().setPersistenceEnabled(true);
     }
 }
