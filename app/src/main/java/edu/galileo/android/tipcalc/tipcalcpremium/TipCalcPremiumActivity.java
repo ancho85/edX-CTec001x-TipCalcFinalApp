77b956165dd06aec3136bf48447e029f28a33d31
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-07-10 13:26:07 -0400

    Custom support actionbar con la calculadora entre los menús (feo)

--- app/src/main/java/edu/galileo/android/tipcalc/tipcalcpremium/TipCalcPremiumActivity.java
+++ app/src/main/java/edu/galileo/android/tipcalc/tipcalcpremium/TipCalcPremiumActivity.java
@@ -1,19 +1,57 @@
 package edu.galileo.android.tipcalc.tipcalcpremium;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import edu.galileo.android.tipcalc.R;
 import edu.galileo.android.tipcalc.TipCalcApp;
 
 public class TipCalcPremiumActivity extends AppCompatActivity {
 
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.tabs)
+    TabLayout tabs;
+    @Bind(R.id.appbar)
+    AppBarLayout appbar;
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.separator)
+    View separator;
+    @Bind(R.id.txtTip)
+    TextView txtTip;
+    @Bind(R.id.container)
+    ViewPager container;
+    @Bind(R.id.main_content)
+    CoordinatorLayout mainContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calc_premium);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
     }
 
     @Override
